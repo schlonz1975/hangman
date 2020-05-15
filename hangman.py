007f8a982e27c97ed9b17e1c9a655e4f09d857ed
Jens Mairich <jmschlonz@gmail.com>
2020-05-15 15:33:23 +0200

    hangman v0.2 letter printed correctly for one guess

--- hangman.py
+++ hangman.py
@@ -7,18 +7,17 @@ words = ["hello", "boat", "character", "justice", "marina", "football"]
 random.shuffle(words)
 secret_word = list(words[0])
 max_guesses = 11
-# write empty lines for the word's characters
-for item in range(len(secret_word)):
-    print("_", end=" ")
-
-# main game loop
+char_guessed = False
+player_guess = None
+# loop for each guess
 for guess in range(max_guesses):
-    print()
+    # check each letter if guessed and print accordingly
+    for letter in range(len(secret_word)):
+        if secret_word[letter] == player_guess:
+            print(player_guess, end=" ")
+        else:
+            print("_", end=" ")
+
     player_guess = str.lower(input("Make a guess: "))
-    if player_guess in secret_word:
-        print("Great")
-
-
-
 
 
