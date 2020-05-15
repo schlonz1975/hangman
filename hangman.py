c6584b0bd77565d6a0cd232472f5f72c0d024fe4
Jens Mairich <jmschlonz@gmail.com>
2020-05-15 17:25:27 +0200

    hangman v0.3 new approach, introduced var "display" as copy of secret words

--- hangman.py
+++ hangman.py
@@ -6,18 +6,54 @@ words = ["hello", "boat", "character", "justice", "marina", "football"]
 # store the item at index[0] in a new list "secret_word"
 random.shuffle(words)
 secret_word = list(words[0])
-max_guesses = 11
-char_guessed = False
+guesses = 0
+# char_guessed = False
 player_guess = None
-# loop for each guess
-for guess in range(max_guesses):
-    # check each letter if guessed and print accordingly
+display = secret_word.copy()
+
+# "display" gets turned into dashes for initial print
+for item in range(len(display)):
+    display[item] = "_"
+
+# print(display)
+
+# main game loop
+while guesses < 11:
+    # code executed per guess
     for letter in range(len(secret_word)):
         if secret_word[letter] == player_guess:
-            print(player_guess, end=" ")
-        else:
-            print("_", end=" ")
+            display[letter] = secret_word[letter]
 
-    player_guess = str.lower(input("Make a guess: "))
+        for item in display:
+            print(str(item), end=" ")
+
+        print()
+        player_guess = str.lower(input("Make a guess: "))
+        guesses += 1
+
+
+
+
+
+
+# loop for each guess
+
+
+
+
+
+
+
+
+
+# for guess in range(max_guesses):
+#     # check each letter if guessed and print accordingly
+#     for letter in range(len(secret_word)):
+#         if secret_word[letter] == player_guess:
+#             print(player_guess, end=" ")
+#         else:
+#             print("_", end=" ")
+#
+#     player_guess = str.lower(input("Make a guess: "))
 
 
